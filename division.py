a1146e6ce6ca2cdb97f7d770deb0cf62909b9241
Tri Putra <tp031000@gmail.com>
2021-04-21 20:08:48 +0700

    Menambahkan file division.py

---
+++ division.py
@@ -0,0 +1,9 @@
+# Masukkan angka
+no1 = float(input("Masukkan angka pertama: "))
+no2 = float(input("Masukkan angka kedua: "))
+
+# Operasi membagikan
+bag = no1 / no2;
+
+# Output
+print("Hasil pembagian dari angka yang diberikan adalah:",bag)
